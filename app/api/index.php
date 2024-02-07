03abf55f2cd6658d4fa89dd4b12c06faf63790bc
Michal Bubílek <mik.bubilek@gmail.com>
2024-02-07 17:36:27 +0100

    header, CORS

--- app/api/index.php
+++ app/api/index.php
@@ -1,6 +1,12 @@
 <?php
 error_reporting(E_ALL);
 mb_internal_encoding("UTF-8");
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json');
+
 require_once('conf.php');
 require_once(DIR_APP . 'utils/Loader.php');
 spl_autoload_register('Loader::loadClass');
