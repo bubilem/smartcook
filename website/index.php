3d6aa777569da41a069c478e0b886ef0c6ed2d26
Michal Bubílek <mik.bubilek@gmail.com>
2024-05-15 16:43:20 +0200

    favicon

--- website/index.php
+++ website/index.php
@@ -12,6 +12,17 @@ require "parts/init.php";
   <meta name="description" content="<?php include "parts/description.php"; ?>" />
   <meta name="keywords" content="<?php include "parts/keywords.php"; ?>" />
   <title><?php include "parts/title.php"; ?></title>
+
+  <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="img/favicon/site.webmanifest">
+  <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#ee4477">
+  <link rel="shortcut icon" href="img/favicon/favicon.ico">
+  <meta name="msapplication-TileColor" content="#373030">
+  <meta name="msapplication-config" content="img/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+
   <link rel="stylesheet" href="css/style.min.css" />
 </head>
 
--- website/parts/conf.php
+++ website/parts/conf.php
@@ -1,6 +1,6 @@
 <?php
 class Conf
 {
-    const BASE = "https://www.smartcook-project.eu/";//http://www/smartcook/website/
+    const BASE = "http://www/smartcook/website/"; //https://www.smartcook-project.eu/
     const MONTHS = [1 => "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 }
