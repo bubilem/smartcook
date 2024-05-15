abc61f010fe928a5493500c754ac949ee0b0a04e
Michal Bubílek <mik.bubilek@gmail.com>
2024-05-15 20:44:55 +0200

    aside update

--- website/parts/aside.php
+++ website/parts/aside.php
@@ -4,6 +4,7 @@
         <a href="">Home page</a>
         <a href="https://github.com/bubilem/smartcook/tree/main/docs/api">API - documentation</a>
         <a href="https://www.smartcook-project.eu/api">API - application</a>
+        <a href="https://www.smartcook-project.eu/client">CLIENT - application</a>
         <a href="https://www.skolavdf.cz">VOŠ, SPŠ a SOŠ Varnsdorf</a>
     </nav>
     <?php
