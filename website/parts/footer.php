526819948caf8e8fd93a7ca87e13bc2c8f25f7ab
Michal Bubílek <mik.bubilek@gmail.com>
2024-05-27 19:17:32 +0200

    logos

--- website/parts/footer.php
+++ website/parts/footer.php
@@ -1,5 +1,10 @@
 <footer>
     <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> SmartCook</p>
+        <p>&copy;<?php echo date('Y'); ?> SmartCook</p>
+        <div class="fgrid">            
+            <img src="img/eu_funded_en.webp" alt="eu funded logo">
+            <img src="img/skolavdf.webp" alt="skolavdf logo">
+            <img src="img/granatowe_poprawione_krzywe.webp" alt="granatowe poprawione krzywe logo">
+        </div>
     </div>
 </footer>
