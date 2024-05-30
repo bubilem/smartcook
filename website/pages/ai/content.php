29eae894320b37973914fb4adc8f19ecc89bb60a
Michal Bubílek <mik.bubilek@gmail.com>
2024-05-30 18:11:26 +0200

    api diagram, ai

---
+++ website/pages/ai/content.php
@@ -0,0 +1,29 @@
+<article>
+  <h1>We use AI</h1>
+  <p>We use different AI models in practice.</p>
+  <p>Artificial intelligence (AI) is revolutionizing the way students approach their work, 
+    offering tools that enhance efficiency and creativity. In the realm of software development, 
+    extensions for Visual Studio Code, such as Codeium, are proving invaluable. These AI-powered 
+    tools assist with coding by suggesting improvements, automating repetitive tasks, and even 
+    writing code snippets, which significantly speeds up the development process. Furthermore, 
+    AI's impact extends beyond code; students are utilizing AI models like DALL-E to generate 
+    images for various purposes. For instance, they create visual representations for demo data 
+    in databases, design backgrounds for client applications, and more. This not only saves time 
+    but also allows for a high degree of customization and personalization, catering to the specific 
+    needs of their projects. The integration of AI into student work is a testament to the technology's 
+    versatility and its potential to be a game-changer in educational environments and beyond.</p>
+  <div class="gallery">
+    <img src="pages/ai/smartcook.webp" alt="smartcook" />    
+    <img src="pages/ai/background.webp" alt="background smartcook" />    
+  </div>
+  <div class="gallery column-2">
+    <img src="pages/ai/01.webp" alt="food #1" />
+    <img src="pages/ai/02.webp" alt="food #2" />
+    <img src="pages/ai/03.webp" alt="food #3" />
+    <img src="pages/ai/04.webp" alt="food #4" />
+    <img src="pages/ai/05.webp" alt="food #5" />
+    <img src="pages/ai/06.webp" alt="food #6" />
+    <img src="pages/ai/07.webp" alt="food #7" />
+    <img src="pages/ai/08.webp" alt="food #8" />
+  </div>
+</article>
---
+++ website/pages/api-diagram/content.php
@@ -0,0 +1,16 @@
+<article>
+  <h1>API Diagram</h1>
+  <p>Diagram of how our SmartCook API works.</p>
+  <p>Information Technology students in Varnsdorf embarked on a task to create a communication schema for 
+    an Application Programming Interface (API). This API 
+    was designed to respond to URL queries with JSON formatted data, enabling the manipulation of a database 
+    of recipes. To ensure security and integrity, these queries required proper authorization and signature 
+    verification. The API's architecture allowed it to act as a server to other applications, which in turn acted 
+    as clients to the API, while simultaneously serving as a server to their own users. This dual functionality 
+    underscored the API's versatility, allowing for endless possibilities in its application 
+    and integration into various systems. The project not only provided a practical solution for database 
+    management but also served as a valuable learning experience in API design and security protocols for the students.</p>
+  <div class="gallery">
+    <img src="pages/api-diagram/api-diagram.png" alt="api diagram" />    
+  </div>
+</article>
