55d5987fa8766e28a02c3ecd1e11eaa843271b69
Michal Bubílek <mik.bubilek@gmail.com>
2025-01-05 13:38:21 +0100

    website footer + eu logos

--- website/parts/footer.php
+++ website/parts/footer.php
@@ -1,10 +1,13 @@
 <footer>
     <div class="container">
         <p>&copy;<?php echo date('Y'); ?> SmartCook</p>
-        <div class="fgrid">            
-            <img src="img/eu_funded_en.webp" alt="eu funded logo">
-            <img src="img/skolavdf.webp" alt="skolavdf logo">
-            <img src="img/granatowe_poprawione_krzywe.webp" alt="granatowe poprawione krzywe logo">
+        <div class="fgrid">
+            <img src="img/eu_funded_en_blue.webp" alt="Funded by the European Union.">
+            <img src="img/skolavdf.webp" alt="skolavdf - logo">
+            <img src="img/granatowe_poprawione_krzywe.webp" alt="granatowe poprawione krzywe - logo">
         </div>
+        <p>Funded by the European Union. Views and opinions expressed are however those of the author(s) only and do not
+            necessarily reflect those of the European Union or the European Education and Culture Executive Agency
+            (EACEA). Neither the European Union nor EACEA can be held responsible for them.</p>
     </div>
 </footer>
